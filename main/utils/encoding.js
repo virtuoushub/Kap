7dc60d46521f7be77c022dc15c23ebde18ab33b4
Peter Colapietro <petercolapietro@gmail.com>
2020-10-02 08:33:28 -0400

    style(whitespace): fix whitespace errors
    breaking CI
    See: https://app.circleci.com/pipelines/github/wulkano/Kap/168/workflows/047525cc-f95b-41aa-bf6e-be40c377aa7c/jobs/1721/parallel-runs/0/steps/0-103

--- main/utils/encoding.js
+++ main/utils/encoding.js
@@ -15,11 +15,11 @@ const getEncoding = async filePath => {
     await execa(ffmpegPath, ['-i', filePath]);
   } catch (error) {
     const errorText = /.*: Video: (.*?) \(.*/.exec(error.stderr);
-    
+
     if (!errorText) {
       throw error;
     }
-    
+
     return errorText[1];
   }
 };
